f92d7ff45b467319702d950284b903e4209e4532
Jeffrey Ng <jeffreymng@netzero.net>
2016-10-01 14:13:50 -0400

    add completion time to tasks table

--- db/migrate/20161001180702_create_tasks.rb
+++ db/migrate/20161001180702_create_tasks.rb
@@ -3,6 +3,7 @@ class CreateTasks < ActiveRecord::Migration[5.0]
     create_table :tasks do |t|
       t.string :name, null: false, limit: 128
       t.boolean :completed, null: false, default: false
+      t.datetime :completion_time
       t.references :user, foreign_key: true, index: true, null: false
     end
   end
