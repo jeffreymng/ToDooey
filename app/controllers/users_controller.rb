9285a86446827ca3f7c91c3d4d89bf475a1f9be8
Jeffrey Ng <jeffreymng@netzero.net>
2016-10-01 22:50:28 -0400

    add routes and start erb pages for user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    if logged_in?
+      redirect_to root_url
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      flash[:success] = "Account created."
+      redirect_to root_url
+    else
+      @errors = @user.errors.full_messages
+      flash.now[:danger] = "Could not create an account."
+      render 'new'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   email_regex = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,63}\z/i
   password_format = /\A.*(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).*\z/
 
-  validates :username, uniqeness: true, presence: true, length: { in: 6..64 }
+  validates :username, uniqueness: true, presence: true, length: { in: 6..64 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: email_regex }
   validates :password, format: { with: password_format,
                        message: "must contain at least 1 capital letter, 1 lowercase letter, 1 number, and 1 special character" },
